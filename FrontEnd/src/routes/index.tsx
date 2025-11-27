e401b4a32c84d575ea1cb9be92e4de9236825a07
khoindSE180124 <khoind2004@gmail.com>
2025-11-27 19:08:16 +0700

    feat: integrate Google Analytics for page view tracking and add useAnalytics hook

--- FrontEnd/src/routes/index.tsx
+++ FrontEnd/src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routesConfig } from "./routes.config";
 import { Toaster } from "sonner";
+import { useAnalytics } from "@/hooks/google/useAnalytics";
 
 const AppRoutes = () => {
   const router = createBrowserRouter(routesConfig);
+  useAnalytics(); // track route changes
   return (
     <>
       <Toaster position="top-center" closeButton={true} expand={false} />
